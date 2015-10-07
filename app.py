faafc7a5f7aac74f6ff7f7ce19c8f39164be1794
bb071988 <bb071988@gmail.com>
2015-10-07 02:30:17 +0000

    testing push to just dev

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/<name>')
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
     app.run()  # uncomment this to run heroku
+    def hello_name(name):
+        return "Hello {}".format(name)
     # port = int(os.environ.get('PORT', 8080))  # comment these to run heroku
     # app.run(host='0.0.0.0', port=port)  
